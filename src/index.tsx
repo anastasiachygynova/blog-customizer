723822d931af4132801fee1fc12fa8d077487275
Anastasia Chygynova <anastasia_chygynova@mail.ru>
2025-08-07 10:08:56 +0300

    на ревью

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, useRef, useEffect } from 'react';
 import clsx from 'clsx';
-
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
+import { ArrowButton } from 'src/ui/arrow-button';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,53 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const initialArticleState = useRef(defaultArticleState);
+	const [articleState, setArticleState] = useState(defaultArticleState);
+	const [isFormOpen, setIsFormOpen] = useState(false);
+	const sidebarRef = useRef<HTMLDivElement>(null);
+
+	const handleReset = () => {
+		setArticleState(initialArticleState.current);
+	};
+
+	useEffect(() => {
+		if (!isFormOpen) return;
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsFormOpen(false);
+			}
+		};
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, [isFormOpen]);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArrowButton
+				isOpen={isFormOpen}
+				onClick={() => setIsFormOpen(!isFormOpen)}
+			/>
+			<ArticleParamsForm
+				ref={sidebarRef}
+				isOpen={isFormOpen}
+				articleState={articleState}
+				onApply={setArticleState}
+				initialArticleState={initialArticleState.current}
+				onReset={handleReset}
+			/>
 			<Article />
 		</main>
 	);
